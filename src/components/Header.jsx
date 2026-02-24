0a31590335a72694744806e888ce63c905344ca6
valecristiano <valeriamdcristiano@gmail.com>
2026-02-24 14:15:29 +0300

    M1 - input and submit events

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [userSearch, setUserSearch] = useState("");
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit", userSearch);
+
+    // axios.get().then((res)=>
+    //   console.log(res.data);
+
+    // )
+  };
+
   return (
     <header className="bg-light">
-      <div className="container p-2">
-        <h1>Welcome</h1>
-      </div>
+      <section className="container">
+        <div className="row">
+          <div className="col-6 p-2">
+            <h1>BoolFlix</h1>
+          </div>
+          <div className="col-6">
+            <form onSubmit={formSubmit} className="row row-cols-lg-auto g-3 justify-content-end align-items-center m-auto">
+              <div className="col-12">
+                <div className="input-group">
+                  <label className="col-4 form-label m-auto" htmlFor="filmSearch">
+                    Ricerca titoli
+                  </label>
+                  <input value={userSearch} onChange={(e) => setUserSearch(e.target.value)} type="text" className="form-control" id="filmSearch" />
+                </div>
+              </div>
+              <div className="col-12">
+                <button type="submit" className="btn btn-dark">
+                  Cerca
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </header>
   );
+}
+
+{
+  /* <div class="col-12">
+    <label class="visually-hidden" for="inlineFormSelectPref">Preference</label>
+    <select class="form-select" id="inlineFormSelectPref">
+      <option selected>Choose...</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+  </div> */
 }
